dddf718e5332e7cc20111b8f092dd5f369c664e6
shahilgohtar <90614266+shahilgohtar@users.noreply.github.com>
2026-06-30 12:01:41 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = n 
+        
+        while True:
+            slow = self.square_sum(slow)
+            fast = self.square_sum(self.square_sum(fast))
+
+            if slow == fast:
+                break
+        return slow == 1
+        
+    def square_sum(self,n):
+        total  = 0
+
+        while n > 0:
+            digit = n % 10
+            total += digit * digit
+            n //= 10
+        return total
+
+        
+
+
+        
